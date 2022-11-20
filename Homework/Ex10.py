07adc41f6832488444a96c0edf6e0545de216787
Kawaiskiy <dmitry.seidov@yandex.ru>
2022-11-20 20:18:07 +0300

    hi

--- Homework/Ex10.py
+++ Homework/Ex10.py
@@ -1,4 +1,6 @@
-phrase = input("Set the phrase: ")
+class Ex10:
 
-assert len(phrase) <= 15, f"Input phrase length more than 15 symbols: {len(phrase)}"
-print(f"Input phrase length: {len(phrase)}")
+    phrase = input("Set the phrase: ")
+
+    assert len(phrase) <= 15, f"Input phrase length more than 15 symbols: {len(phrase)}"
+    print(f"Input phrase length: {len(phrase)}")
